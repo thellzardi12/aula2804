4f6dadef6bf7347b2096b5606a97b15ab6adf0b4
kavs1346 <130669072+kavs1346@users.noreply.github.com>
2023-05-04 08:50:17 -0300

    commit truco

--- CalculadoraSimples.java
+++ CalculadoraSimples.java
@@ -22,7 +22,21 @@ public class CalculadoraSimples{
             op = entrada.nextInt();
 
             if(op == 1){
+                //Kauan
                 System.out.println("\n\t\t\t -- Soma --\n");
+
+                //Entrada
+                System.out.println("Informe N1: ");
+                n1 = entrada.nextInt();
+                System.out.println("Informe N2: ");
+                n2 = entrada.nextInt();
+
+                //Processamento
+                total = n1 + n2;
+
+                //Saída
+                System.out.printf("%d + %d = %d", n1, n2, total);
+
             } else if(op == 2){
                 System.out.println("\n\t\t\t -- Subtração --n");
             } else if(op == 3){
